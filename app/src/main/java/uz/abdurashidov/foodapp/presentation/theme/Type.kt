57c746f0b477d953bed4f04c65b61bd3da48a41d
abdurashidovvv <abdurashidovsaydullo35@gmail.com>
2024-07-24 13:54:09 +0500

    Add Lora Fonts

--- app/src/main/java/uz/abdurashidov/foodapp/presentation/theme/Type.kt
+++ app/src/main/java/uz/abdurashidov/foodapp/presentation/theme/Type.kt
@@ -2,9 +2,11 @@ package uz.abdurashidov.foodapp.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import uz.abdurashidov.foodapp.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -32,3 +34,8 @@ val Typography = Typography(
     )
     */
 )
+
+val LoraMedium = FontFamily(listOf(Font(resId = R.font.lora_medium)))
+val LoraBold = FontFamily(listOf(Font(resId = R.font.lora_bold)))
+val LoraRegular = FontFamily(listOf(Font(resId = R.font.lora_regular)))
+val LoraSemiBold = FontFamily(listOf(Font(resId = R.font.lora_semibold)))
