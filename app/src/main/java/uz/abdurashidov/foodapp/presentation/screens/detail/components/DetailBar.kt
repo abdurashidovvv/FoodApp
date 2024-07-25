7550d81150a53df8ae3bba45beb4e2de401c71f5
abdurashidovvv <abdurashidovsaydullo35@gmail.com>
2024-07-25 08:56:04 +0500

    DetailScreen Bar

---
+++ app/src/main/java/uz/abdurashidov/foodapp/presentation/screens/detail/components/DetailBar.kt
@@ -0,0 +1,31 @@
+package uz.abdurashidov.foodapp.presentation.screens.detail.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import uz.abdurashidov.foodapp.R
+
+@Composable
+fun DetailBar(modifier: Modifier = Modifier) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier.fillMaxWidth().padding(15.dp)
+    ) {
+        Image(painter = painterResource(id = R.drawable.back_ic), contentDescription = null, modifier = Modifier.size(27.dp))
+        Image(painter = painterResource(id = R.drawable.notification_ic), contentDescription = null, modifier =Modifier.size(27.dp))
+    }
+}
+
+@Preview
+@Composable
+private fun DetailBarPreview() {
+    DetailBar()
+}
